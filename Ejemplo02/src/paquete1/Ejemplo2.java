2c643a652fb5d73cc59d255e9a54cc1b965d4b1d
Josuee16 <josuexpardoch@gmail.com>
2023-11-01 16:47:37 -0500

    Actividad en Clase

--- Ejemplo02/src/paquete1/Ejemplo2.java
+++ Ejemplo02/src/paquete1/Ejemplo2.java
@@ -30,7 +30,9 @@ public class Ejemplo2 {
         System.out.println("Ingrese nota de asignatura 1");
         notaAsignatura1 = entrada.nextDouble();
         System.out.println("Ingrese nombre de asignatura 2");
-        entrada.nextLine(); // limpieza de buffer
+        entrada.nextLine(); /* La limpieza buffer se la realiza cuando despues
+        de una entrada de enteros se pone otra entrada de cadena
+        */
         nombreAsignatura2 = entrada.nextLine();
         System.out.println("Ingrese nota de asignatura 2");
         notaAsignatura2 = entrada.nextDouble();
